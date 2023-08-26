b53b1dbdca9e6daf8847eabd7ad8650a3178258a
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-08-26 20:50:22 +0800

    Fixed build bugs for linux

--- src/window.py
+++ src/window.py
@@ -1,3 +1,5 @@
+import tkinter
+
 import styles
 import widget
 import widgets
@@ -21,35 +23,48 @@ class Window:
 
         self.name = name
 
+        self.error = False
+
         if os.environ.get('DISPLAY', '') == '':
             print('No display found. Using :0.0')
             os.environ.__setitem__('DISPLAY', ':0.0')
 
-        self._window = customtkinter.CTk()
-        self._window.title(self.name)
-        self._window.geometry("500x500" if "size" not in options else options["size"])
-        self._window.resizable(False if "x-resizable" not in options else options["x-resizable"], False if "y-resizeable" not in options else options["y-resizable"])
+        try:
+            self._window = customtkinter.CTk()
+        except tkinter.TclError:
+            print("Failed to create window. Make sure you have a display.")
+            self.error = True
 
-        customtkinter.set_appearance_mode(self.style["appearance_mode"] if "appearance_mode" in self.style else "system")
-        if "default_color_theme" in self.style: customtkinter.set_default_color_theme(self.style["default_color_theme"])
-        customtkinter.set_widget_scaling(self.style["widget_scaling"] if "widget_scaling" in self.style else 1)
-        customtkinter.set_window_scaling(self.style["window_scaling"] if "window_scaling" in self.style else 1)
+        if not self.error:
+            self._window.title(self.name)
+            self._window.geometry("500x500" if "size" not in options else options["size"])
+            self._window.resizable(False if "x-resizable" not in options else options["x-resizable"], False if "y-resizeable" not in options else options["y-resizable"])
 
-        self._items = []
+            customtkinter.set_appearance_mode(self.style["appearance_mode"] if "appearance_mode" in self.style else "system")
+            if "default_color_theme" in self.style: customtkinter.set_default_color_theme(self.style["default_color_theme"])
+            customtkinter.set_widget_scaling(self.style["widget_scaling"] if "widget_scaling" in self.style else 1)
+            customtkinter.set_window_scaling(self.style["window_scaling"] if "window_scaling" in self.style else 1)
+
+            self._items = []
 
     def main_loop(self):
-        self._window.mainloop()
+        if not self.error:
+            self._window.mainloop()
 
     def add_widget(self, widget: any, options={}):
-        widget = widget._ctk
-        if "place" in options:
-            widget.place(relx=options["place"]["relx"], rely=options["place"]["rely"], anchor=options["place"]["anchor"])
-        else:
-            widget.pack()
+        if not self.error:
+            widget = widget._ctk
+            if "place" in options:
+                widget.place(relx=options["place"]["relx"], rely=options["place"]["rely"], anchor=options["place"]["anchor"])
+            else:
+                widget.pack()
 
-        self._items.append(widget)
+            self._items.append(widget)
 
     # slotter to get when used like this: window
     def __call__(self, *args, **kwargs):
-        return self._window
+        if not self.error:
+            return self._window
+        else:
+            return None
 
