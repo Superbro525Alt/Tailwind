a9868cc8ced776a304b5a574e3924bd409f1e64b
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-08-29 14:11:01 +0800

    Added more statistics

--- src/main.py
+++ src/main.py
@@ -8,14 +8,16 @@ import util
 from window import Window
 from widgets import *
 import graphing
+import statistics
 
 if __name__ == "__main__":
-    window = Window(None, "window", util.WindowProperties.empty())
-    window.add_widget(Button(style=util.Style.empty(), properties={"text": "Test Button"}, binds={}, window=window))
-    window.add_widget(Label(style=util.Style.empty(), properties={"text": "Test Label"}, binds={}, window=window))
-    graph = graphing.Graph(graphing.GraphOptions("x", "y", [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "graph", util.ImageScale(500, 500), "line"), window)
-    window.add_widget(graph.display(), util.PlaceData(0.5, 0.5, util.CenterAnchor().get_anchor()))
-    graph2 = graphing.Graph(graphing.GraphOptions("x", "r", [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "graph1", util.ImageScale(500, 500), "line"), window)
-    window.add_widget(graph2.display(), util.PlaceData(0.5, 0.5, util.CenterAnchor().get_anchor()))
-    window.main_loop()
+    print(statistics.standard_deviation([1, 2, 3, 4, 5]))
+    #window = Window(None, "window", util.WindowProperties.empty())
+    #window.add_widget(Button(style=util.Style.empty(), properties={"text": "Test Button"}, binds={}, window=window))
+    #window.add_widget(Label(style=util.Style.empty(), properties={"text": "Test Label"}, binds={}, window=window))
+    #graph = graphing.Graph(graphing.GraphOptions("x", "y", [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "graph", util.ImageScale(500, 500), "line"), window)
+    #window.add_widget(graph.display(), util.PlaceData(0.5, 0.5, util.CenterAnchor().get_anchor()))
+    #graph2 = graphing.Graph(graphing.GraphOptions("x", "r", [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "graph1", util.ImageScale(500, 500), "line"), window)
+    #window.add_widget(graph2.display(), util.PlaceData(0.5, 0.5, util.CenterAnchor().get_anchor()))
+    #window.main_loop()
 
--- src/statistics.py
+++ src/statistics.py
@@ -23,3 +23,30 @@ def z_score(data, x):
 
 def z_scores(data):
     return [z_score(data, x) for x in data]
+
+def percentile(data, p):
+    return data[math.floor(len(data) * p)]
+
+def quartiles(data):
+    return [percentile(data, 0.25), percentile(data, 0.5), percentile(data, 0.75)]
+
+def interquartile_range(data):
+    return quartiles(data)[2] - quartiles(data)[0]
+
+def outliers(data):
+    return [x for x in data if x < quartiles(data)[0] - 1.5 * interquartile_range(data) or x > quartiles(data)[2] + 1.5 * interquartile_range(data)]
+
+def remove_outliers(data):
+    return [x for x in data if x not in outliers(data)]
+
+def covariance(data1, data2):
+    return sum([(x - mean(data1)) * (y - mean(data2)) for x, y in zip(data1, data2)]) / len(data1)
+
+def correlation(data1, data2):
+    return covariance(data1, data2) / (standard_deviation(data1) * standard_deviation(data2))
+
+def least_squares_regression(data1, data2):
+    return covariance(data1, data2) / variance(data1)
+
+def least_squares_regression_line(data1, data2):
+    return lambda x: least_squares_regression(data1, data2) * x + mean(data2) - least_squares_regression(data1, data2) * mean(data1)
