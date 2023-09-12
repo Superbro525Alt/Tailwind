3c1f4ec01b68773bd8145cff20457dfd8cc7b60d
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-12 11:21:37 +0800

    Fixed module importing bugs

--- tailwind/main.py
+++ tailwind/main.py
@@ -6,7 +6,9 @@ from tailwind.widgets import *
 import tailwind.util as util
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent_dir + "/tailwind")
+sys.path.append(parent_dir)
+
+print(parent_dir)
 
 if util.is_main_thread(__name__):
     def create():
