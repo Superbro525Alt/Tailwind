8b3c469b35fe333469abdf956f3ee586953d153a
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-13 11:35:47 +0800

    Added LAN networking

--- tailwindall/main.py
+++ tailwindall/main.py
@@ -164,17 +164,18 @@ def example1():
     window.main_loop()
 
 if util.is_main_thread(__name__):
-    d = database.LANDatabase(65053)
-
-    d.start()
-
-    c = client.LANClient(d.host, 65053)
-
-    c.connect()
-
-    c.send("set|path/to/data|test3")
-    print(c.send("get|path/to/data"))
-
-    d.close()
-
+    pass
+    # d = database.LANDatabase(65053)
+    #
+    # d.start()
+    #
+    # c = client.LANClient(d.host, 65053)
+    #
+    # c.connect()
+    #
+    # c.send("set|path/to/data|test3")
+    # print(c.send("get|path/to/data"))
+    #
+    # d.close()
+    #
 
