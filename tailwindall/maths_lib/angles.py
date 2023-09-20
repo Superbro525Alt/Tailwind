6d7eb3bc4bac2d70f66743e1876c564bb332c3a9
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-20 14:09:54 +0800

    Added base for sorting points of a shape (works for any amount of points)

--- tailwindall/maths_lib/angles.py
+++ tailwindall/maths_lib/angles.py
@@ -1,5 +1,14 @@
 import math, numpy as np
 
-def angle_between_lines(line1_start, line1_end, line2_start, line2_end):
-    return np.degrees(np.arccos(np.dot((line1_end - line1_start), (line2_end - line2_start)) / (
-            np.linalg.norm(line1_end - line1_start) * np.linalg.norm(line2_end - line2_start))))
+def angle_between_lines(point: tuple, line1: tuple, line2: tuple, origin: tuple) -> float:
+    """
+    Returns the angle between two lines.
+    :param point:
+    :param line1:
+    :param line2:
+    :param origin:
+    :return:
+    """
+    return np.degrees(np.arctan2(point[1] - origin[1], point[0] - origin[0]) - np.arctan2(line1[1] - origin[1],
+                                                                                          line1[0] - origin[0]) - (
+                              np.arctan2(line2[1] - origin[1], line2[0] - origin[0]))) % 360 - 180
--- tailwindall/maths_lib/shapes.py
+++ tailwindall/maths_lib/shapes.py
@@ -1,2 +1,51 @@
-def get_shape_from_points(points: list, clockwise: bool = True):
-    raise NotImplementedError("This function is not implemented yet.")
+import sys, os
+
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import tailwindall.util as util
+import tailwindall.classes_lib.classes as classes
+import tailwindall.maths_lib.angles as angles
+
+
+
+class Shape(classes.BaseObject):
+    def __init__(self, points: list):
+        self.points = points
+
+        self.points = ShapeSorter.sort(self)
+
+        self.name = ShapeName.find(self)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+class ShapeName(classes.BaseObject):
+    @classmethod
+    def find(cls, shape: Shape) -> str:
+        points = shape.points
+
+        return util.null
+
+class ShapeSorter(classes.BaseObject):
+    @classmethod
+    def sort(cls, shape: Shape) -> list:
+        """
+        Sorts a shape in a clockwise direction with trigonometry.
+        :param shape:
+        :return:
+        """
+        return sorted(shape.points, key=lambda point: angles.angle_between_lines(point, shape.points[0], (
+        shape.points[0][0], shape.points[0][1] + 1), shape.points[0]))
+
+
+
+def get_shape_from_points(points: list) -> Shape:
+    """
+    Returns a shape from a list of points.
+    """
+    return Shape(points)
+
+
+if util.is_main_thread(__name__):
+    print(get_shape_from_points([(0, 0), (1, 0), (1, 1), (0, 1)]))
