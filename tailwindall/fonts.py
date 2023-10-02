0955d8d7b96f6c19acc8941dd093b3a48643a7b9
= <paul.hodges@student.education.wa.edu.au>
2023-10-02 12:44:55 +0800

    Added logging and fixed errors with importing fonts

--- tailwindall/fonts.py
+++ tailwindall/fonts.py
@@ -5,12 +5,16 @@ sys.path.append(parent_dir)
 
 import pyglet
 import tailwindall.util as util
+import tailwindall.logger as logger
 
 def init():
     paths = []
-    for file in os.listdir(os.path.join(util.path(), "nerd_fonts")):
-        if file.endswith(".ttf"):
-            pyglet.font.add_file(os.path.join(util.path(), "nerd_fonts", file))
-            paths.append(os.path.join(util.path(), "nerd_fonts", file))
+    try:
+        for file in os.listdir(os.path.join(util.path(), "nerd_fonts")):
+            if file.endswith(".ttf"):
+                pyglet.font.add_file(os.path.join(util.path(), "nerd_fonts", file))
+                paths.append(os.path.join(util.path(), "nerd_fonts", file))
+    except Exception as e:
+        logger.error(f"An error occurred while loading fonts: {e}")
 
     return paths
